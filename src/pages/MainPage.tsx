ae70447dc7f47e3e538b5221b492b90fe64e476d
jina <ewenhoop44yy@gmail.com>
2025-07-14 22:59:34 +0900

    진짜재설정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,1 +1,14 @@
+import React from "react";
 
+const MainPage: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-gray-800">Welcome to MainPage!</h1>
+      <p className="mt-4 text-lg text-gray-600">
+        This is your main page component.
+      </p>
+    </div>
+  );
+};
+
+export default MainPage;
